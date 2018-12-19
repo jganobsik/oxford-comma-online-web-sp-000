8092bedecc65f008d8af48608de6b03b1afa13b9
Joe Ganobsik <jganobsik@gmail.com>
2018-12-19 00:24:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
 def oxford_comma(array)
   last_word = array.last
   with_and = "and " + last_word
+  if array.length == 1 
+    return array
+  elsif array.length == 2 
+     array.join(" and ")
+   else 
   array.pop
   array << with_and
+  array.join(" ,")
   
-array.join(" ,")
 end
